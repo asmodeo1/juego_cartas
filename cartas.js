d794bc1327b8a9d07ecbafe3d63acbe7a9a25bd2
Juan <alonso.jad@gmail.com>
2024-05-20 13:50:49 +0200

    Add files via upload

--- cartas.js
+++ cartas.js
@@ -1,25 +1,62 @@
 
-function mover(evt) {
-    let fuerza = document.getElementById("fuerza");
-    const elegidos = document.getElementById("elegidos");
-    if (evt.currentTarget.parentNode.id === "elegidos") {
-        const disponibles = document.getElementById("disponibles");
-        disponibles.appendChild(evt.currentTarget);
-        fuerza.textContent = parseInt(fuerza.textContent) - parseInt(evt.currentTarget.dataset.fuerza);
-    } else if (elegidos.querySelector(`[data-planeta=${evt.currentTarget.dataset.planeta}`)) {
-        document.getElementById("mensaje").style.display = "block";
+function moverCarta(evento) {
+    const elegidos = document.getElementById('elegidos');
+    const disponibles = document.getElementById('disponibles');
+    const fuerza = document.getElementById("fuerza");
+    // currentTarget es el elemento sobre el que se produjo el evento
+    const carta = evento.currentTarget;
+    // parentNode es el nodo padre de un elemento
+    if(carta.parentNode == elegidos) {
+        // Como está en elegidos lo muevo a disponibles
+        // appendChild mueve un elemento si está en otro nodo
+        disponibles.appendChild(carta);
+        // Resto la fuerza que hay en las cartas elegidas
+        // Con dataset accedemos al valor del atributo data- indicado (en este caso data-fuerza)
+        // Solo al sumar hay que hacer los parseInt
+        fuerza.textContent = fuerza.textContent - carta.dataset.fuerza;
+
     } else {
-        elegidos.appendChild(evt.currentTarget);
-        fuerza.textContent = parseInt(fuerza.textContent) + parseInt(evt.currentTarget.dataset.fuerza);
+        // Vamos a ver si hay una carta del mimo planeta en elegidos
+        const cartas = elegidos.getElementsByClassName("carta");
+        for (const c of cartas) {
+            if(c.dataset.planeta == carta.dataset.planeta) {
+                // Mostramos el mensaje de error
+                const mensaje = document.getElementById('mensaje');
+                mensaje.style.display = "block";
+                // Salimos de la función para que no haga luego el appendChild
+                return;
+            }
+        }
+        /* Otra manera de comprobar si ya hay una carta de ese planeta en elegidos 
+       if(elegidos.querySelector(`[data-planeta='${carta.dataset.planeta}']`) != null) {
+         // Mostramos el mensaje de error
+         const mensaje = document.getElementById('mensaje');
+         mensaje.style.display = "block";
+         // Salimos de la función para que no haga luego el appendChild
+         return;
+       }
+       */
+
+        // Como está en disponibles lo muevo a elegidos
+        elegidos.appendChild(carta);
+        // Sumamos la fuerza que hay en las cartas elegidas
+        fuerza.textContent = parseInt(fuerza.textContent) + parseInt(carta.dataset.fuerza);
     }
 }
 
 function cerrarMensaje() {
-    document.getElementById("mensaje").style.display = "none";
+    const mensaje = document.getElementById('mensaje');
+    mensaje.style.display = "none";
 }
 
-const cartas = document.getElementsByClassName("carta");
+const cartas = document.getElementsByClassName('carta');
 for (const carta of cartas) {
-    carta.addEventListener("click", mover);
+    carta.addEventListener("click", moverCarta);
+    /* Como no hemos añadido el planeta de cada carta en HTML, lo añadimos ahora con
+    JavaScript */
+    const p = document.createElement("p")
+    p.textContent = carta.dataset.planeta;
+    carta.appendChild(p);
 }
-document.getElementById("cerrar").addEventListener("click", cerrarMensaje);
+
+document.getElementById('cerrar').addEventListener("click", cerrarMensaje);
